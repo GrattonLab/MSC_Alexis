91bbf57a371007fbc7c45e463b8797983009cf3f
Alexis Porter <alexis.porter1313@gmail.com>
2019-12-04 10:23:31 -0600

    MVPA

---
+++ analysis/code/MVPA/svm_MSC_batch.m
@@ -0,0 +1,33 @@
+load('/Users/aporter1350/Box/Quest_Backup/MSC/TaskFC/FC_Parcels/mem/MSC02_parcel_corrmat.mat');
+MSC02_mem=parcel_corrmat;
+load('/Users/aporter1350/Box/Quest_Backup/MSC/TaskFC/FC_Parcels/rest/MSC02_parcel_corrmat.mat');
+MSC02_rest=parcel_corrmat;
+%MSC02_mem(isinf(MSC02_mem)|isnan(MSC02_mem)) = 0; % Replace NaNs and infinite values with zeros
+%MSC02_rest(isinf(MSC02_rest)|isnan(MSC02_rest)) = 0; % Replace NaNs and infinite values with zeros
+%concatenating memory and rest along third dimension first 10 days are task
+%then rest
+MSC02_mem_rest=cat(3, MSC02_mem, MSC02_rest)
+
+%test along different subject
+load('/Users/aporter1350/Box/Quest_Backup/MSC/TaskFC/FC_Parcels/mem/MSC03_parcel_corrmat.mat');
+MSC03_mem=parcel_corrmat;
+load('/Users/aporter1350/Box/Quest_Backup/MSC/TaskFC/FC_Parcels/rest/MSC03_parcel_corrmat.mat');
+MSC03_rest=parcel_corrmat;
+%MSC03_mem(isinf(MSC03_mem)|isnan(MSC03_mem)) = 0; % Replace NaNs and infinite values with zeros
+%MSC03_rest(isinf(MSC03_rest)|isnan(MSC03_rest)) = 0; % Replace NaNs and infinite values with zeros
+%concatenating memory and rest along third dimension first 10 days are task
+%then rest
+MSC03_mem_rest=cat(3, MSC03_mem, MSC03_rest)
+
+
+%svm script
+results=svm_scripts_beta(MSC02_mem_rest, [ones(10,1); -ones(10,1)],0,0,0,0)
+%svm script with test set being another sub
+results=svm_scripts_beta(MSC02_mem_rest, [ones(10,1); -ones(10,1)],0,MSC03_mem_rest,[ones(10,1); -ones(10,1)],0)
+
+%svm script with test set being another task
+load('/Users/aporter1350/Box/Quest_Backup/MSC/TaskFC/FC_Parcels/motor/MSC02_parcel_corrmat.mat');
+MSC02_motor=parcel_corrmat;
+%MSC02_motor(isinf(MSC02_motor)|isnan(MSC02_motor)) = 0; % Replace NaNs and infinite values with zeros
+
+results=svm_scripts_beta(MSC02_mem_rest, [ones(10,1); -ones(10,1)],0,MSC02_motor,[ones(10,1)],0)
--- analysis/code/workbench_parcel_analysis/corrmat_parcels_batch.m
+++ analysis/code/workbench_parcel_analysis/corrmat_parcels_batch.m
@@ -2,26 +2,26 @@ clear;
 clear all;
 
 
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC02_mem_corrmat.mat', 'MSC02_mem')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC05_mem_corrmat.mat', 'MSC05_mem')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC06_mem_corrmat.mat', 'MSC06_mem')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC02_mem_corrmat.mat', 'MSC02_mem')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC05_mem_corrmat.mat', 'MSC05_mem')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC06_mem_corrmat.mat', 'MSC06_mem')
 
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC02_mix_corrmat.mat', 'MSC02_mixed')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC05_mix_corrmat.mat', 'MSC05_mixed')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC06_mix_corrmat.mat', 'MSC06_mixed')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC02_mix_corrmat.mat', 'MSC02_mixed')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC05_mix_corrmat.mat', 'MSC05_mixed')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC06_mix_corrmat.mat', 'MSC06_mixed')
 
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC02_motor_corrmat.mat', 'MSC02_motor')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC05_motor_corrmat.mat', 'MSC05_motor')
-%load('/Users/Alexis/Documents/MSC_Alexis/analysis/data/FC_Parcels/MSC06_motor_corrmat.mat', 'MSC06_motor')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC02_motor_corrmat.mat', 'MSC02_motor')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC05_motor_corrmat.mat', 'MSC05_motor')
+%load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC06_motor_corrmat.mat', 'MSC06_motor')
 
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC02_rest_corrmat.mat', 'MSC02_rest')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC05_rest_corrmat.mat', 'MSC05_rest')
-load('/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/data/FC_Parcels/MSC06_rest_corrmat.mat', 'MSC06_rest')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC02_rest_corrmat.mat', 'MSC02_rest')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC05_rest_corrmat.mat', 'MSC05_rest')
+load('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/MSC06_rest_corrmat.mat', 'MSC06_rest')
 
 
-f=figure_corrmat_network_generic(MSC02_mem, atlas_parameters('Parcels','/Users/aporter1350/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC02'))
+f=figure_corrmat_network_generic(MSC02_mem, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC02'))
 colormapeditor
-saveas(f, '/Users/aporter1350/Box/My Box Notes/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_mem.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_mem.png')
 
 f=figure_corrmat_network_generic(MSC05_mem, atlas_parameters('Parcels','/Users/aporter1350/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC05'))
 colormapeditor
@@ -80,37 +80,37 @@ MSC05_motorMrest=MSC05_motor-MSC05_rest
 
 
 %task minus rest corr maps 
-f=figure_corrmat_network_generic(MSC02_memMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC02'))
+f=figure_corrmat_network_generic(MSC02_memMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC02'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_memMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_memMrest.png')
 
-f=figure_corrmat_network_generic(MSC05_memMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC05'))
+f=figure_corrmat_network_generic(MSC05_memMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC05'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_05_memMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_05_memMrest.png')
 
-f=figure_corrmat_network_generic(MSC06_memMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC06'))
+f=figure_corrmat_network_generic(MSC06_memMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC06'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_06_memMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_06_memMrest.png')
 
-f=figure_corrmat_network_generic(MSC02_mixMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC02'))
+f=figure_corrmat_network_generic(MSC02_mixMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC02'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_mixMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_mixMrest.png')
 
-f=figure_corrmat_network_generic(MSC05_mixMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC05'))
+f=figure_corrmat_network_generic(MSC05_mixMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC05'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_05_mixMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_05_mixMrest.png')
 
-f=figure_corrmat_network_generic(MSC06_mixMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC06'))
+f=figure_corrmat_network_generic(MSC06_mixMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC06'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_06_mixMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_06_mixMrest.png')
 
-f=figure_corrmat_network_generic(MSC02_motorMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC02'))
+f=figure_corrmat_network_generic(MSC02_motorMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC02'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_motorMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_02_motorMrest.png')
 
-f=figure_corrmat_network_generic(MSC05_motorMrest, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC05'))
+f=figure_corrmat_network_generic(MSC05_motorMrest, atlas_parameters('Parcels','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/FC_Parcels/','MSC05'))
 colormapeditor
-saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_05_motorMrest.png')
+saveas(f, '/Users/aporter1350/Desktop/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_05_motorMrest.png')
 %f=figure_corrmat_network_generic(MSC06_motor, atlas_parameters('Parcels','/Users/Alexis/Box/Quest_Backup/Atlases/Evan_parcellation/','MSC06'))
 %colormapeditor
 %saveas(f, '/Users/Alexis/Documents/MSC_Alexis/analysis/output/images/heatmap_corr/MSC_06_motor.png')
