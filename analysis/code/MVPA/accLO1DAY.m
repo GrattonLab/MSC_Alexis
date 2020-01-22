e6b151ec6aa1ef6728778260ce26bb85a1aa9b6f
aporter1350 <alexis.porter1313@gmail.com>
2020-01-22 14:49:28 -0600

    acc fixed

--- analysis/code/MVPA/accLO1DAY.m
+++ analysis/code/MVPA/accLO1DAY.m
@@ -1,20 +1,15 @@
 function accLO1DAY(sub)
     trainList={'mem','mixed','motor'}
     %create empty cell to store
-    C=cell(3,3)
+    C=cell(1,3)
     for i=1:3
-        for j=1:3
-            if i==j
-                C{j,i}=0
-            else
-            %open results file
-                results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/LO1DAY/results_mat/' trainList{i} sub '.mat']
-                load(results)
-            %calculate the accuracy 
-                acc=mean((sum(results.predictedTestLabels(1,:)==1)+sum(results.predictedTestLabels(2,:)==-1))./36)
-                C{j,i}=acc
-            end
-        end
+        %open results file
+        results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/LO1DAY/results_mat/' trainList{i} sub '.mat']
+        load(results)
+        %calculate the accuracy 
+        acc=mean((sum(results.predictedTestLabels(1,:)==1)+sum(results.predictedTestLabels(2,:)==-1))./36)
+        C{1,i}=acc
+
 end
 T=cell2table(C)
 T.Properties.VariableNames=trainList
@@ -22,4 +17,5 @@ T.Properties.VariableNames=trainList
 sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/LO1DAY/' sub '_acc.csv']
 writetable(T, sname, 'WriteRowNames', true)
 %type sname
+end 
 
