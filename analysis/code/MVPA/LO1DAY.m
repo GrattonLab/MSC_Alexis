70382760b6c5c49e1dc0d0397b96a2c58b667d8e
Alexis Porter <alexis.porter1313@gmail.com>
2020-01-31 12:43:17 -0600

    analysis

--- analysis/code/MVPA/LO1DAY.m
+++ analysis/code/MVPA/LO1DAY.m
@@ -17,32 +17,37 @@ function LO1DAY(sub)
     restFile=load(restFC);
     rest=restFile.parcel_corrmat;
     taskList={motor,mem,mixed};
-    myFolder='~/Desktop/MSC_Alexis/analysis/data/mvpa_data/'; %defining working directory
+    taskListNames = {'motor', 'mem', 'mixed'}
+    %myFolder='~/Desktop/MSC_Alexis/analysis/data/mvpa_data/'; %defining working directory
     
     for i=1:length(taskList)
         taskFC=taskList{i};
         restFC=rest;
         %select good days
-        good_task = ~isnan(squeeze(sum(sum(taskFC,2),1)))
-        good_rest = ~isnan(squeeze(sum(sum(restFC,2),1)))
+        good_task = ~isnan(squeeze(sum(sum(taskFC,2),1)));
+        good_rest = ~isnan(squeeze(sum(sum(restFC,2),1)));
+        only_good = good_task .* good_rest
+        taskFC_clean = taskFC(:,:,only_good);
+        restFC_clean= restFC(:,:, only_good);
         
         %select train and test indices
-        nsamples = size(taskFC,3);
+        nsamples = size(taskFC_clean, 3)
         sample_inds = [1:nsamples];
         for t = 1:nsamples
             test_ind = t;
-            train_ind = setdiff(set(sample_inds),set(test_ind));
+            train_ind = setdiff((sample_inds),(test_ind));
             %leaving a day out to use for testing later
-            rest_train=r(:,:,train_ind);
-            task_train=t(:,:,train_ind);
-            rest_test=r(:,:,test_ind);
-            task_test=t(:,:,test_ind);
+            rest_train=restFC_clean(:,:,train_ind);
+            task_train=taskFC_clean(:,:,train_ind);
+            rest_test=restFC_clean(:,:,test_ind);
+            task_test=taskFC_clean(:,:,test_ind);
             train=cat(3, task_train, rest_train);
             test=cat(3, task_test, rest_test);
             %training across 9 days testing on one day
-            results=svm_scripts_beta(train, [ones(9,1); -ones(9,1)],0,test,[ones(length(test_set),1); -ones(length(test_set),1)],0);
-            mean((sum(results.predictedTestLabels(1,:)==1)+sum(results.predictedTestLabels(2,:)==-1))./2)
-            saveName=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/LO1DAY/results_mat/' taskList{i} sub '.mat']
+            results=svm_scripts_beta(train, [ones(size(task_train,3),1); -ones(size(rest_train,3),1)],0,test,[ones(size(task_test,3),1); -ones(size(rest_test,3),1)],0);
+            (sum(results.predictedTestLabels(1,:)==1)+sum(results.predictedTestLabels(2,:)==-1))./numel(results.predictedTestLabels);   
+            saveName=[strcat('~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/LO1DAY/results_mat/day_', num2str(test_ind), taskListNames{i}, sub, '.mat')]
             save(saveName, 'results')
+        end 
     end
 end 
