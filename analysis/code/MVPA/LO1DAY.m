f432faf830dc87abe6a8c8e682c2c1dd193f9611
Alexis Porter <alexis.porter1313@gmail.com>
2020-01-31 16:50:27 -0600

    guess the days

--- analysis/code/MVPA/LO1DAY.m
+++ analysis/code/MVPA/LO1DAY.m
@@ -21,7 +21,7 @@ function LO1DAY(sub)
     %myFolder='~/Desktop/MSC_Alexis/analysis/data/mvpa_data/'; %defining working directory
     
     for i=1:length(taskList)
-        taskFC=taskList{i};
+        taskFC=motor;
         restFC=rest;
         %select good days
         good_task = ~isnan(squeeze(sum(sum(taskFC,2),1)));
@@ -29,10 +29,17 @@ function LO1DAY(sub)
         only_good = logical(good_task .* good_rest);
         taskFC_clean = taskFC(:,:,only_good);
         restFC_clean= restFC(:,:, only_good);
-        
+        %subjects 2, 5, 6, then look at 1 or 4 based on nans, 7, 8, 9
+        %exclude, 3, 10 weird
         %select train and test indices
         nsamples = size(taskFC_clean, 3)
         sample_inds = [1:nsamples];
+        %labels = eye(10) would need 10 columns but 40 rows to use all task
+        %data 
+        %labels(labels==0)=-1
+        %labels = [labels;labels]
+        %results.predictedLabels==[[1:10]';[1:10]']
+        %results=svm_scripts_beta(t, labels,0,0,0,0);
         for t = 1:nsamples
             test_ind = t;
             train_ind = setdiff((sample_inds),(test_ind));
@@ -41,10 +48,10 @@ function LO1DAY(sub)
             task_train=taskFC_clean(:,:,train_ind);
             rest_test=restFC_clean(:,:,test_ind);
             task_test=taskFC_clean(:,:,test_ind);
-            train=cat(3, task_train, rest_train);
+            train=cat(3, task_train, rest_train); 
             test=cat(3, task_test, rest_test);
             %training across 9 days testing on one day
-            results=svm_scripts_beta(train, [ones(size(task_train,3),1); -ones(size(rest_train,3),1)],0,test,[ones(size(task_test,3),1); -ones(size(rest_test,3),1)],0);
+            results=svm_scripts_beta(train, [ones(size(task_train,3),1); -ones(size(rest_train,3),1)],0,test,[ones(size(task_test,3),1); -ones(size(rest_test,3),1)],0); %to arrange in pairs options=1
             (sum(results.predictedTestLabels(1,:)==1)+sum(results.predictedTestLabels(2,:)==-1))./numel(results.predictedTestLabels);   
             saveName=[strcat('~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/LO1DAY/results_mat/day_', num2str(test_ind), taskListNames{i}, sub, '.mat')]
             save(saveName, 'results')
