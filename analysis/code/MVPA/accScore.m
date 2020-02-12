e41f0c5f23722b4fc73bc3e0e78f3c64c227a079
aporter1350 <alexis.porter1313@gmail.com>
2020-02-12 15:44:59 -0600

    timeseries and only good data

--- analysis/code/MVPA/accScore.m
+++ analysis/code/MVPA/accScore.m
@@ -9,12 +9,12 @@ function accScore(sub)
                 C{j,i}=0
             else
             %open results file
-                %results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/within_sub_test/results_mat/test_' predictList{j} '_train_' trainList{i} sub '.mat']
+                results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/within_sub_test/results_mat/only_good/test_' predictList{j} '_train_' trainList{i} sub '.mat'];
             %open randomly permuted file
-                results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/random_permutation_test/results_mat/test_' predictList{j} '_train_' trainList{i} sub '.mat']
-                load(results)
+                %results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/random_permutation_test/results_mat/test_' predictList{j} '_train_' trainList{i} sub '.mat']
+                load(results);
             %calculate the accuracy 
-                acc=mean((sum(results.predictedTestLabels(1:10,:)==1))./10)
+                acc=mean(sum(results.predictedTestLabels)./size(results.predictedTestLabels,1).*100);
                 C{j,i}=acc
             end
         end
@@ -22,8 +22,8 @@ end
 T=cell2table(C)
 T.Properties.VariableNames=trainList
 T.Properties.RowNames=predictList
-sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/random_permutation_test/' sub '_accRandom.csv']
-%sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/within_sub_test/' sub '_acc.csv']
+%sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/random_permutation_test/' sub '_accRandom.csv']
+sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/within_sub_test/' sub '_onlyGood_acc.csv']
 writetable(T, sname, 'WriteRowNames', true)
 %type sname
 
--- analysis/code/MVPA/acc_betweenScore.m
+++ analysis/code/MVPA/acc_betweenScore.m
@@ -1,22 +1,23 @@
 function acc_betweenScore(task)
-    trainList={'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC08','MSC09','MSC10'};
-    predictList={'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC08','MSC09','MSC10'};
+    trainList={'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC10'};
+    predictList={'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC10'};
     %create empty cell to store
     %based on the amount of nans in the testing dataset will impact how to
     %calculate the accuracy, should have probably written in something to
     %note this in the previous script...could have something output
-    C=cell(10,10);
-    for i=1:10
-        for j=1:10
+    C=cell(8,8);
+    for i=1:length(trainList)
+        for j=1:length(predictList)
             if i==j
                 C{j,i}=0;
             else
             %open results file
-                results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/between_sub_test/results_mat/train_' trainList{i} '_test_' predictList{j} task '.mat'];
+                results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/between_sub_test/results_mat/only_good/train_' trainList{i} '_test_' predictList{j} task '.mat'];
                 load(results);
             %calculate the accuracy 
-                aa = [results.predictedTestLabels(1:10,:)==1;results.predictedTestLabels(11:20,:)==-1];
-                mean(aa(:));
+                taskcut=size(results.predictedTestLabels,1)/2;
+                aa = [results.predictedTestLabels(1:taskcut,:)==1;results.predictedTestLabels(taskcut+1:end,:)==-1];
+                acc=mean(aa(:));
                 C{j,i}=acc;
             end
         end
@@ -24,7 +25,7 @@ end
 T=cell2table(C)
 T.Properties.VariableNames=trainList
 T.Properties.RowNames=predictList
-sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/between_sub_test/' task '_acc.csv']
+sname=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/between_sub_test/' task '_onlyGood_acc.csv']
 writetable(T, sname, 'WriteRowNames', true)
 %type sname
 
