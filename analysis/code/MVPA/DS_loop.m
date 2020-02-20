26ddde1b1d1a05361043f9c4dc977605ae015e8e
aporter1350 <alexis.porter1313@gmail.com>
2020-02-20 14:42:50 -0600

    between sub more time

--- analysis/code/MVPA/DS_loop.m
+++ analysis/code/MVPA/DS_loop.m
@@ -1,4 +1,5 @@
-task={'mem', 'mixed', 'motor'}
+%task={'mem', 'mixed', 'motor'}
+task={'mem', 'motor'}
 for i=1:length(task)
     DS(task{i})
 end
--- analysis/code/MVPA/acc_betweenScore.m
+++ analysis/code/MVPA/acc_betweenScore.m
@@ -1,6 +1,8 @@
 function acc_betweenScore(task)
     trainList={'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC10'};
     predictList={'MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC10'};
+    %trainList={'MSC01','MSC02','MSC04','MSC05'};
+    %predictList={'MSC01','MSC02','MSC04','MSC05'};
     C=cell(8,8);
     for i=1:length(trainList)
         for j=1:length(predictList)
@@ -9,6 +11,7 @@ function acc_betweenScore(task)
             else
             %open results file
                 results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/between_sub_test/results_mat/only_good/train_' trainList{i} '_test_' predictList{j} task '.mat'];
+                %results=['~/Desktop/MSC_Alexis/analysis/output/results/MVPA_mat/between_sub_test/results_mat/only_good/timesplit_train_' trainList{i} '_test_' predictList{j} task '.mat'];
                 load(results);
             %calculate the accuracy 
                 taskcut=size(results.predictedTestLabels,1)/2;
