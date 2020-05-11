4fd01eef4493c68439923c1b0eaed8993de7b11d
aporter1350 <alexis.porter1313@gmail.com>
2020-05-11 15:09:24 -0500

    modifying pipelines

---
+++ analysis/code/mL_rsfMRI/classify.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from sklearn.svm import LinearSVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RidgeClassifier
+import numpy as np
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_validate
+from sklearn import metrics
+import itertools
+#import other python scripts for further anlaysis
+import reshape
+import plotFW
+import results
+# Initialization of directory information:
+thisDir = os.path.expanduser('~/Desktop/MSC_Alexis/analysis/')
+dataDir = thisDir + 'data/mvpa_data/'
+outDir = thisDir + 'output/'
+# Subjects and tasks
+taskList=['mixed', 'motor','mem']
+subList=['MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC10']
+#all possible combinations of subs and tasks
+subsComb=(list(itertools.permutations(subList, 2)))
+tasksComb=(list(itertools.permutations(taskList, 2)))
+#DS combination
+DSvars=list(itertools.product(list(subsComb),list(taskList)))
+##SS combination
+SSvars=list(itertools.product(list(subList),list(tasksComb)))
+#BS combination
+BSvars=list(itertools.product(list(subsComb),list(tasksComb)))
+
+
+""" run_prediction initializes what type of analysis you would like to do
+and what classifier you would like to use. For now classifier options are svm:linear svm, logreg: logistic
+regression, and ridge:ridge regression. Analysis is the type of analysis you wanted
+to run. DS--different subject same task; SS--same subject different task;
+BS--different subject different task. Each analysis will concatenate across
+subjects and make a dataframe. If CVscores=True calculate accuracy of folds for subject and task.
+If FW is true will collect all necessary feature weights and plot or save then
+into the appropriate format. If plotACC=True will plot the accuracy as heatmaps.
+If statsACC=True will make tables of the mean and sd for each task."""
+def run_prediction(classifier, analysis, FW=False, CVscores=False, plotACC=False, statsACC=False):
+    if CVscores is True:
+        classifyCV(classifier)
+    elif analysis=='DS':
+        classifyDS(classifier, analysis, FW, plotACC, statsACC)
+    elif analysis=='SS':
+        classifySS(classifier, analysis, FW, plotACC, statsACC)
+    elif analysis=='BS':
+        classifyBS(classifier, analysis, FW, plotACC, statsACC)
+    else:
+        print('Error: You didnt specify what analysis')
+
+
+def classifyDS(classifier, analysis, FW, plotACC, statsACC):
+    acc_scores_per_task=[]
+    tmp_df=pd.DataFrame(DSvars, columns=['sub','task'])
+    dfDS=pd.DataFrame()
+    dfDS[['train_sub','test_sub']]=pd.DataFrame(tmp_df['sub'].tolist())
+    dfDS['task']=tmp_df['task']
+    for index, row in dfDS.iterrows():
+        score=model(classifier, analysis,FW, train_sub=row['train_sub'], test_sub=row['test_sub'], train_task=row['task'], test_task=row['task'])
+        acc_scores_per_task.append(score)
+    dfDS['acc']=acc_scores_per_task
+    if plotACC is True:
+        results.plotACC(dfDS, classifier, analysis)
+    else:
+        print('skipping over heatmaps for accuracy')
+    if statsACC is True:
+        results.statsACC(dfDS, classifier, analysis)
+    else:
+        print('skipping over making stat tables for accuracy, saving accuracy as csv')
+    #save accuracy
+    dfDS.to_csv(outdir+'results/'+classifier+'/acc/DS/acc.csv')
+    #running this script will output x, y
+    #x_train, y_train=reshape.concateFC(taskFC, restFC)
+#if SS store df of cross validation folds
+def classifySS(classifier,analysis, FW, plotACC, statsACC):
+    acc_scores_per_task=[]
+    tmp_df=pd.DataFrame(SSvars, columns=['sub','task'])
+    dfSS=pd.DataFrame()
+    dfSS[['train_task','test_task']]=pd.DataFrame(tmp_df['task'].tolist())
+    dfSS['sub']=tmp_df['sub']
+    for index, row in dfSS.iterrows():
+        score=model(classifier, analysis,FW, train_sub=row['sub'], test_sub=row['sub'], train_task=row['train_task'], test_task=row['test_task'])
+        acc_scores_per_task.append(score)
+    dfSS['acc']=acc_scores_per_task
+    if plotACC is True:
+        results.plotACC(dfSS, classifier, analysis)
+    else:
+        print('skipping over heatmaps for accuracy')
+    if statsACC is True:
+        results.statsACC(dfSS, classifier, analysis)
+    else:
+        print('skipping over making stat tables for accuracy, saving accuracy as csv')
+    #save accuracy
+    dfSS.to_csv(outdir+'results/'+classifier+'/acc/SS/acc.csv')
+
+
+def classifyBS(classifier, analysis, FW, plotACC, statsACC):
+    acc_scores_per_task=[]
+    tmp_df=pd.DataFrame(BSvars, columns=['sub','task'])
+    dfBS=pd.DataFrame()
+    dfBS[['train_task','test_task']]=pd.DataFrame(tmp_df['task'].tolist())
+    dfBS[['train_sub', 'test_sub']]=pd.DataFrame(tmp_df['sub'].tolist())
+    for index, row in dfBS.iterrows():
+        score=model(classifier, analysis,FW, train_sub=row['train_sub'], test_sub=row['test_sub'], train_task=row['train_task'], test_task=row['test_task'])
+        acc_scores_per_task.append(score)
+    dfBS['acc']=acc_scores_per_task
+    if plotACC is True:
+        results.plotACC(dfBS, classifier, analysis)
+    else:
+        print('skipping over heatmaps for accuracy')
+    if statsACC is True:
+        results.statsACC(dfBS, classifier, analysis)
+    else:
+        print('skipping over making stat tables for accuracy, saving accuracy as csv')
+    #save accuracy
+    dfBS.to_csv(outdir+'results/'+classifier+'/acc/BS/acc.csv')
+    #running this script will output x, y
+    #x_train, y_train=reshape.concateFC(taskFC, restFC)
+def model(classifier, analysis,FW, train_sub, test_sub, train_task, test_task):
+    if classifier=='SVC':
+        clf=LinearSVC()
+    elif classifier=='logReg':
+        clf=LogisticRegression(solver = 'lbfgs')
+    elif classifier=='ridge':
+        clf=RidgeClassifier()
+    else:
+        print('Error: You didnt specify what classifier')
+    taskFC=reshape.matFiles(dataDir+train_task+'/'+train_sub+'_parcel_corrmat.mat')
+    restFC=reshape.matFiles(dataDir+'rest/'+train_sub+'_parcel_corrmat.mat')
+    x_train, y_train=reshape.concateFC(taskFC, restFC)
+    #if your subs are the same dont include rest...avoid overfitting!!
+    if train_sub==test_sub:
+        x_test=reshape.matFiles(dataDir+test_task+'/'+test_sub+'_parcel_corrmat.mat')
+        taskSize=x_test.shape[0]
+        y_test=np.ones(taskSize, dtype=int)
+    else:
+        test_taskFC=reshape.matFiles(dataDir+test_task+'/'+test_sub+'_parcel_corrmat.mat')
+        test_restFC=reshape.matFiles(dataDir+'rest/'+test_sub+'_parcel_corrmat.mat')
+        x_test, y_test=reshape.concateFC(test_taskFC, test_restFC)
+    clf.fit(x_train,y_train)
+    folds=taskFC.shape[0]
+    CVscores=cross_val_score(clf,x_train,y_train, cv=folds)
+    if FW is True:
+        coef=clf.coef_
+        plotFW.feature_plots(coef, classifier, analysis, train_task, train_sub)
+    else:
+        pass
+    predict=clf.predict(x_test)
+    #Get accuracy of model
+    ACCscores=clf.score(x_test,y_test)
+    return ACCscores
+#Calculate acc of cross validation within sub within task
+def classifyCV(classifier):
+    avg_CV=[]
+    if classifier=='SVC':
+        clf=LinearSVC()
+    elif classifier=='logReg':
+        clf=LogisticRegression(solver = 'lbfgs')
+    elif classifier=='ridge':
+        clf=RidgeClassifier()
+    else:
+        print('Error: You didnt specify what classifier')
+    for task in taskList:
+        acc_scores_per_task=[]
+        cvTable=[]
+        for sub in subList:
+            taskFC=reshape.matFiles(dataDir+task+'/'+sub+'_parcel_corrmat.mat')
+            restFC=reshape.matFiles(dataDir+'rest/'+sub+'_parcel_corrmat.mat')
+            folds=taskFC.shape[0]
+            x_train, y_train=reshape.concateFC(taskFC, restFC)
+            CVscores=cross_val_score(clf, x_train, y_train, cv=folds)
+            mu=CVscores.mean()
+            acc_scores_per_task.append(mu)
+            cv_tmp_df=pd.DataFrame({sub:CVscores})
+            cvTable.append(cv_tmp_df)
+    #acc per fold per sub
+        tmp_df=pd.DataFrame({'sub':subList, task:acc_scores_per_task}).set_index('sub')
+        avg_CV.append(tmp_df)
+        cvTable=pd.concat(cvTable, axis=1)
+    #saving cv per folds if debugging
+        cvTable.to_csv(outDir+'results/'+classifier+'/acc/DS/'+sub+'_cvTable_folds.csv')
+    #average acc per sub per tasks
+    avg_CVTable=pd.concat(avg_CV, axis=1)
+    #plot as heatmaps
+    results.plotACC(avg_CVTable, classifier, 'CV')
+    avg_CVTable.to_csv(outDir+'results/'+classifier+'/acc/DS/cvTable_avg.csv')
---
+++ analysis/code/mL_rsfMRI/plotFW.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#Imports
+import numpy as np
+import scipy.io as spio
+import matplotlib.pyplot as plt
+import pandas as pdimport sys
+import os
+import sys
+thisDir = os.path.expanduser('~/Desktop/MSC_Alexis/analysis/')
+dataDir = thisDir + 'data/mvpa_data/'
+outDir = thisDir + 'output/'
+def feature_plots(coef, classifier='which', analysis='type', train_task='task', train_sub='i'):
+    #convert to 2d symmetrical matrix
+    nrois=333
+    ds=np.zeros((nrois, nrois))
+    ds[np.triu_indices(ds.shape[0], k = 1)] = coef
+    ds = ds + ds.T
+    #get atlas you want to use
+    Parcel_params = loadParcelParams('Gordon333',thisDir+'data/Parcel_info/')
+    #make pretty fig
+    #vmin=np.amin(ds)
+    #vmax=np.amax(ds)
+    fig = figure_corrmat(ds,Parcel_params, clims=(-.002,.002))
+    fig.savefig(outDir+"images/"+classifier+"/fw/" +analysis+ "/" +task+ "_" +sub+ ".png", bbox_inches='tight')
+
+def loadParcelParams(roiset,datadir):
+    """ This function loads information about the ROIs and networks.
+    For now, this is only set up to work with 333 Gordon 2014 Cerebral Cortex regions
+    Inputs:
+    roiset = string naming roi type to get parameters for (e.g. 'Gordon333')
+    datadir = string path to the location where ROI files are stored
+    Returns:
+    Parcel_params: a dictionary with ROI information stored in it
+    """
+    import scipy.io as spio
+    #initialize a dictionary where info will be stored
+    Parcel_params = {}
+
+    # put some info into the dict that will work for all roi sets
+    Parcel_params['roiset'] = roiset
+    dataIn_types = {'dmat','mods_array','roi_sort','net_colors'}
+    for dI in dataIn_types:
+          dataIn = spio.loadmat(datadir + roiset + '_' + dI + '.mat')
+          Parcel_params[dI] = np.array(dataIn[dI])
+    Parcel_params['roi_sort'] = Parcel_params['roi_sort'] - 1 #orig indexing in matlab, need to subtract 1
+
+    #transition points and centers for plotting
+    transitions,centers = compute_trans_centers(Parcel_params['mods_array'],Parcel_params['roi_sort'])
+    Parcel_params['transitions'] = transitions
+    Parcel_params['centers'] = centers
+
+    # some ROI specific info that needs to be added by hand
+    # add to this if you have a new ROI set that you're using
+    if roiset == 'Gordon333':
+        Parcel_params['dist_thresh'] = 20 #exclusion distance to not consider in metrics
+        Parcel_params['num_rois'] = 333
+        Parcel_params['networks'] = ['unassign','default','visual','fp','dan','van','salience',
+                                         'co','sm','sm-lat','auditory','pmn','pon']
+    else:
+        raise ValueError("roiset input is recognized.")
+
+    return Parcel_params
+def compute_trans_centers(mods_array,roi_sort):
+    """ Function that computes transitions and centers of networks for plotting names
+    Inputs:
+    mods_array: a numpy vector with the network assignment for each ROI (indexed as a number)
+    roi_sort: ROI sorting ordered to show each network in sequence
+    Returns:
+    transitions: a vector with transition points between networks
+    centers: a vector with center points for each network
+    """
+
+    mods_sorted = np.squeeze(mods_array[roi_sort])
+    transitions = np.nonzero((np.diff(mods_sorted,axis=0)))[0]+1 #transition happens 1 after
+
+    trans_plusends = np.hstack((0,transitions,mods_array.size)) #add ends
+    centers = trans_plusends[:-1] + ((trans_plusends[1:] - trans_plusends[:-1])/2)
+
+    return transitions,centers
+
+def figure_corrmat(corrmat,Parcel_params, clims=(-1,1)):
+    """ This function will make a nice looking plot of a correlation matrix for a given parcellation,
+    labeling and demarkating networks.
+    Inputs:
+    corrmat: an roi X roi matrix for plotting
+    Parcel_params: a dictionary with ROI information
+    clims: (optional) limits to place on corrmat colormap
+    Returns:
+    fig: a figure handle for figure that was made
+    """
+
+    # some variables for ease
+    roi_sort = np.squeeze(Parcel_params['roi_sort'])
+
+    # main figure plotting
+    fig, ax = plt.subplots()
+    im = ax.imshow(corrmat[roi_sort,:][:,roi_sort],cmap='seismic',vmin=clims[0],vmax=clims[1], interpolation='none')
+    plt.colorbar(im)
+
+    # add some lines between networks
+    for tr in Parcel_params['transitions']:
+        ax.axhline(tr,0,Parcel_params['num_rois'],color='k')
+        ax.axvline(tr,0,Parcel_params['num_rois'],color='k')
+
+    # alter how the tick marks are shown to plot network names
+    ax.set_xticks(Parcel_params['centers'])
+    ax.set_yticks(Parcel_params['centers'])
+    ax.set_xticklabels(Parcel_params['networks'],fontsize=8)
+    ax.set_yticklabels(Parcel_params['networks'],fontsize=8)
+    plt.setp(ax.get_xticklabels(), rotation = 45, ha = 'right', rotation_mode = 'anchor')
+
+    plt.show()
+
+    return fig
+def saveFW(coef,classifier, analysis, task, sub):
+    #convert to 2d symmetrical matrix
+    nrois=333
+    ds=np.zeros((nrois, nrois))
+    ds[np.triu_indices(ds.shape[0], k = 1)] = coef
+    ds = ds + ds.T
+    Parcel_params = loadParcelParams('Gordon333',thisDir+'data/Parcel_info/')
+    roi_sort = np.squeeze(Parcel_params['roi_sort'])
+    #rearrange roi's to be together
+    corrmat=ds[roi_sort,:][:,roi_sort]
+    nrois=list(range(333))
+    nets=[]
+    position=0
+    count=0
+    networks=Parcel_params['networks']
+    t=Parcel_params['transitions']
+    #have to add extra value otherwise error
+    transitions=np.append(t,333)
+    while count<333:
+            if count<=transitions[position]:
+                nets.append(networks[position])
+                count=count+1
+            else:
+                position=position+1
+    df=pd.DataFrame(corrmat, index=[nets, nrois], columns=[nets, nrois])
+    df.to_csv(outDir+"results/"+classifier+"/fw/" +analysis+ "/" +task+ "_" +sub+ ".csv")
+
+    #you'll have to specify that it is a tuple pd.read_csv('test.csv',index_col=[0,1])
+
+#standard deviation of feature weights between folds
+#did not initialize for classify.py since this is for debugging
+def fwFolds(folds, classifier, analysis, task, sub):
+    #concate into useable form
+    fw=np.empty([10,55278])
+    count=0
+    for model in folds['estimator']:
+        i=model.coef_
+        fw[count]=i
+        count=count+1
+    fwSD=np.std(fw, axis=0)
+    nrois=333
+    ds=np.zeros((nrois, nrois))
+    ds[np.triu_indices(ds.shape[0], k = 1)] = fwSD
+    ds = ds + ds.T
+    #get atlas you want to use
+    Parcel_params = loadParcelParams('Gordon333',thisDir+'data/Parcel_info/')
+    #make pretty fig
+    vmin=np.amin(ds)
+    vmax=np.amax(ds)
+    fig = figure_corrmat(ds,Parcel_params, clims=(0, .0002))
+    fig.savefig(outDir+"images/"+classifier+"/fw/" +analysis+ "/fwSD_Folds/" +task+ "_" +sub+ ".png", bbox_inches='tight')
+
+
+
+# In[1]:
+
+
+"""
+import reshape
+import pandas as pd
+import numpy as np
+from sklearn.svm import LinearSVC
+#Load task FC
+taskFC=reshape.matFiles('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/mvpa_data/mem/MSC05_parcel_corrmat.mat')
+#Load rest
+restFC=reshape.matFiles('/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/mvpa_data/rest/MSC05_parcel_corrmat.mat')
+#Create a training dataset targets 1/0 ==task/rest, chunk=#days
+x_train=np.concatenate((taskFC, restFC))
+taskSize=taskFC.shape[0]
+restSize=restFC.shape[0]
+t = np.ones(taskSize, dtype = int)
+r=np.zeros(restSize, dtype=int)
+y_train = np.concatenate((t,r))
+
+svm = LinearSVC()
+svm.fit(x_train, y_train)
+coef = svm.coef_
+#convert to 2d symmetrical matrix
+#nrois=333
+#ds=np.zeros((nrois, nrois))
+#ds[np.triu_indices(ds.shape[0], k = 1)] = coef
+#ds = ds + ds.T
+#Parcel_params = loadParcelParams('Gordon333','/Users/aporter1350/Desktop/MSC_Alexis/analysis/data/Parcel_info/')
+saveFW(coef,'DS', 'test','foo')
+"""
+
+
+# In[ ]:
