4815fea313ca5002c30140271f5ae2fa14817775
aporter1350 <alexis.porter1313@gmail.com>
2021-02-10 20:19:50 -0600

    all task all sess leave one sub out

--- analysis/code/mL_rsfMRI/Predictions/groupAvg.py
+++ analysis/code/mL_rsfMRI/Predictions/groupAvg.py
@@ -91,10 +91,6 @@ def model(train_task, test_task):
             Average accuracy of all folds of a left out subject but tested on a new task
 
     """
-
-    clf=RidgeClassifier()
-    loo = LeaveOneOut()
-    df=pd.DataFrame()
     #nsess x fc x nsub
     ds_T=np.empty((10,55278,7))
     ds_R=np.empty((10,55278,7))
@@ -186,3 +182,123 @@ def model(train_task, test_task):
     df['DS_fold']=btn_scoreList
     DS_score=df['DS_fold'].mean()
     return cv_score, cv_sensitivity, cv_specificity, SS_score, DS_score
+
+
+def allTask():
+    """
+    Leave one subject out, using all tasks but only using one session per model; 28 samples 4Task*7subs*1session
+
+    Parameters
+    -------------
+
+    Returns
+    -------------
+    df : pandas Dataframe
+            Accuracy, sensitivity, specificity scores for each kfold
+
+    """
+    clf=RidgeClassifier()
+    loo = LeaveOneOut()
+    final_df=pd.DataFrame()
+    #nsess x fc x nsub
+    ds_mem=np.empty((10,55278,7))
+    ds_sem=np.empty((10,55278,7))
+    ds_glass=np.empty((10,55278,7))
+    ds_motor=np.empty((10,55278,7))
+    ds_R=np.empty((40,55278,7))
+    count=0
+    #get all subs for a given task
+    for sub in subList:
+        #training task
+        tmp_memFC=reshape.matFiles(dataDir+'mem/'+sub+'_parcel_corrmat.mat')
+        tmp_semFC=reshape.matFiles(dataDir+'semantic/'+sub+'_parcel_corrmat.mat')
+        tmp_glassFC=reshape.matFiles(dataDir+'glass/'+sub+'_parcel_corrmat.mat')
+        tmp_motorFC=reshape.matFiles(dataDir+'motor/'+sub+'_parcel_corrmat.mat')
+        tmp_restFC=reshape.matFiles(dataDir+'rest/corrmats_timesplit/fourths/'+sub+'_parcel_corrmat.mat')
+        #reshape 2d into 3d nsessxfcxnsubs
+        ds_mem[:,:,count]=tmp_memFC
+        ds_sem[:,:,count]=tmp_semFC
+        ds_glass[:,:,count]=tmp_glassFC
+        ds_motor[:,:,count]=tmp_motorFC
+        ds_R[:,:,count]=tmp_restFC
+        count=count+1
+
+
+    #split up by subs not sess
+    sub_splits=np.empty((7,55278))
+    #left out sub
+
+    sen_CV=[]
+    spec_CV=[]
+    accScore_allsess=[]
+    #declare session to work in first 10 sessions
+    for i in range(10):
+        cv_scoreList=[]
+        df=pd.DataFrame()
+        tmp_mem=ds_mem[i,:,:]
+        tmp_mem=tmp_mem.T
+
+        tmp_mot=ds_motor[i,:,:]
+        tmp_mot=tmp_mot.T
+
+        tmp_sem=ds_sem[i,:,:]
+        tmp_sem=tmp_sem.T
+
+        tmp_glass=ds_glass[i,:,:]
+        tmp_glass=tmp_glass.T
+        if i==0:
+            ds_rest=ds_R[:4,:,:]
+        else:
+            startingpoint=i*4
+            endingpoint=startingpoint+4
+            ds_rest=ds_R[startingpoint:endingpoint,:,:]
+    #now do leave one sub out cv
+        for train_index, test_index in loo.split(sub_splits):
+            #train on all sessions 1-6 subs
+            #test on all sessions of one sub
+            train_mem, test_mem=tmp_mem[train_index,:],tmp_mem[test_index,:]
+            train_mot, test_mot=tmp_mot[train_index,:],tmp_mot[test_index,:]
+            train_sem, test_sem=tmp_sem[train_index,:],tmp_sem[test_index,:]
+            train_glass, test_glass=tmp_glass[train_index,:],tmp_glass[test_index,:]
+            #rest
+            train_rest, Xtest_rest=ds_rest[:,:,train_index],ds_rest[:,:,test_index[0]]
+
+            #now reshape data
+            Xtrain_task=np.concatenate((train_mem,train_mot,train_sem,train_glass))
+            Xtest_task=np.concatenate((test_mem,test_mot,test_sem,test_glass))
+            Xtrain_rest=train_rest.reshape(24,55278)
+            #test on a new task of 1-6 subs
+            #test on a new task of left out sub
+            #reshape data into a useable format for mL
+            #6 subs with 10 sessions per 4 task
+
+            #training set
+            taskSize=Xtrain_task.shape[0]
+            restSize=Xtrain_rest.shape[0]
+            t = np.ones(taskSize, dtype = int)
+            r=np.zeros(restSize, dtype=int)
+            x_train=np.concatenate((Xtrain_task,Xtrain_rest))
+            y_train=np.concatenate((t,r))
+            #testing set (left out sub CV)
+            testSize=Xtest_task.shape[0]
+            test_restSize=Xtest_rest.shape[0]
+            test_t = np.ones(testSize, dtype = int)
+            test_r=np.zeros(test_restSize, dtype=int)
+            x_test=np.concatenate((Xtest_task, Xtest_rest))
+            y_test=np.concatenate((test_t,test_r))
+            #test left out sub 10 sessions
+            clf.fit(x_train,y_train)
+            y_pre=clf.predict(x_test)
+            #tn, fp, fn, tp=confusion_matrix(y_test, y_pre).ravel()
+            #specificity= tn/(tn+fp)
+            #sensitivity= tp/(tp+fn)
+            #sen_CV.append(sensitivity)
+            #spec_CV.append(specificity)
+            ACCscores=clf.score(x_test,y_test)
+            cv_scoreList.append(ACCscores)
+        df['cv_fold']=cv_scoreList
+        allsub_per_sess=df['cv_fold'].mean()
+        accScore_allsess.append(allsub_per_sess)
+    final_df['acc_per_sess']=accScore_allsess
+    #df.to_csv(outDir+'allTasks_acc.csv',index=False)
+    return final_df
