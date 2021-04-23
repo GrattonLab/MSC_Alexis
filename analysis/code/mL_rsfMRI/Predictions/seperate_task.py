7fb855b5c9beec4cdcc8378f43ee45ced878ee7d
aporter1350 <alexis.porter1313@gmail.com>
2021-04-23 08:04:03 -0500

    new pred

--- analysis/code/mL_rsfMRI/Predictions/seperate_task.py
+++ analysis/code/mL_rsfMRI/Predictions/seperate_task.py
@@ -18,12 +18,13 @@ import os
 import sys
 import reshape
 from statistics import mean
+from sklearn.metrics import f1_score
 #import other python scripts for further anlaysis
 # Initialization of directory information:
 #thisDir = os.path.expanduser('~/Desktop/MSC_Alexis/analysis/')
 thisDir = os.path.expanduser('~/Desktop/MSC_Alexis/analysis/')
 dataDir = thisDir + 'data/mvpa_data/'
-outDir = thisDir + 'output/results/SVM/acc/'
+outDir = thisDir + 'output/results/acc/'
 # Subjects and tasks
 splitDict=dict([('MSC01',10),('MSC02',10),('MSC03',8),('MSC04',10),('MSC05',10),('MSC06',9),('MSC07',9),('MSC10',10)])
 
@@ -397,11 +398,28 @@ def AllSubFiles(test_sub):
     g_motFC=reshape.matFiles(dataDir+'motor/'+test_sub[6]+'_parcel_corrmat.mat')
     g_restFC=reshape.matFiles(dataDir+'rest/'+test_sub[6]+'_parcel_corrmat.mat')
 
+    memFC=np.concatenate((a_memFC,b_memFC,c_memFC,d_memFC,e_memFC,f_memFC,g_memFC))
+    ymem=np.ones(memFC.shape[0])
 
-    taskFC=np.concatenate((a_memFC,a_semFC,a_glassFC,a_motFC,b_memFC,b_semFC,b_glassFC,b_motFC,c_memFC,c_semFC,c_glassFC,c_motFC,d_memFC,d_semFC,d_glassFC,d_motFC,e_memFC,e_semFC,e_glassFC,e_motFC,f_memFC,f_semFC,f_glassFC,f_motFC,g_memFC,g_semFC,g_glassFC,g_motFC))
+    semFC=np.concatenate((a_semFC,b_semFC,c_semFC,d_semFC,e_semFC,f_semFC,g_semFC))
+    ysem=np.full(semFC.shape[0],2)
+
+    glassFC=np.concatenate((a_glassFC,b_glassFC,c_glassFC,d_glassFC,e_glassFC,f_glassFC,g_glassFC))
+    yglass=np.full(glassFC.shape[0],3)
+
+    motFC=np.concatenate((a_motFC,b_motFC,c_motFC,d_motFC,e_motFC,f_motFC,g_motFC))
+    ymot=np.full(motFC.shape[0],4)
+
+    taskFC=np.concatenate((memFC,semFC,glassFC, motFC))
+    ytask=np.concatenate((ymem,ysem,yglass,ymot))
+    #taskFC=np.concatenate((a_memFC,a_semFC,a_glassFC,a_motFC,b_memFC,b_semFC,b_glassFC,b_motFC,c_memFC,c_semFC,c_glassFC,c_motFC,d_memFC,d_semFC,d_glassFC,d_motFC,e_memFC,e_semFC,e_glassFC,e_motFC,f_memFC,f_semFC,f_glassFC,f_motFC,g_memFC,g_semFC,g_glassFC,g_motFC))
     restFC=np.concatenate((a_restFC,b_restFC,c_restFC,d_restFC,e_restFC,f_restFC,g_restFC))
-
-    return taskFC, restFC
+    test_restSize=restFC.shape[0]
+    testR= np.zeros(test_restSize, dtype = int)
+    testFC=np.concatenate((taskFC,restFC))
+    ytest=np.concatenate((ytask,testR))
+    return testFC,ytest
+    #return taskFC, restFC,ytask, testR
 
 
 def modelAll():
@@ -437,12 +455,12 @@ def modelAll():
         semFC=reshape.matFiles(dataDir+'semantic/'+train_sub[0]+'_parcel_corrmat.mat')
         glassFC=reshape.matFiles(dataDir+'glass/'+train_sub[0]+'_parcel_corrmat.mat')
         motFC=reshape.matFiles(dataDir+'motor/'+train_sub[0]+'_parcel_corrmat.mat')
-        restFC=reshape.matFiles(dataDir+'rest/corrmats_timesplit/fourths/'+train_sub[0]+'_parcel_corrmat.mat') #keep tasks seperated in order to collect the right amount of days
+        restFC=reshape.matFiles(dataDir+'rest/'+train_sub[0]+'_parcel_corrmat.mat') #keep tasks seperated in order to collect the right amount of days
         nsize=restFC.shape[1]
         restFC=np.reshape(restFC,(10,4,nsize)) #reshape to gather correct days
         #test sub
-        test_taskFC,test_restFC=AllSubFiles(test_sub)
-        same_Tsub, same_Rsub, diff_Tsub, diff_Rsub=K_folds(train_sub, clf, memFC,semFC,glassFC,motFC, restFC, test_taskFC,test_restFC)
+        test_taskFC,test_restFC,ytask, testR=AllSubFiles(test_sub)
+        same_Tsub, same_Rsub, diff_Tsub, diff_Rsub=K_folds(train_sub, clf, memFC,semFC,glassFC,motFC, restFC, test_taskFC,test_restFC,ytask, testR)
         tmp['train']=train_sub
         tmp['same_subT']=same_Tsub
         tmp['diff_subT']=diff_Tsub
@@ -472,10 +490,14 @@ def K_folds(train_sub, clf, memFC,semFC,glassFC,motFC, restFC, test_taskFC,test_
     #kf = KFold(n_splits=5,shuffle=True)
     loo = LeaveOneOut()
     number=memFC.shape[1]
-    test_taskSize=test_taskFC.shape[0]
-    test_restSize=test_restFC.shape[0]
-    testT= np.ones(test_taskSize, dtype = int)
-    testR= np.zeros(test_restSize, dtype = int)
+    #test_taskSize=test_taskFC.shape[0]
+    #test_restSize=test_restFC.shape[0]
+    #testT= np.ones(test_taskSize, dtype = int)
+    #testR= np.zeros(test_restSize, dtype = int)
+    ymem=np.ones(memFC.shape[0])
+    ysem=np.full(semFC.shape[0],2)
+    yglass=np.full(glassFC.shape[0],3)
+    ymot=np.full(motFC.shape[0],4)
     CVTacc=[]
     CVRacc=[]
     df=pd.DataFrame()
@@ -486,32 +508,171 @@ def K_folds(train_sub, clf, memFC,semFC,glassFC,motFC, restFC, test_taskFC,test_
     split=np.empty((session, 55278))
     for train_index, test_index in loo.split(split):
         memtrain, memval=memFC[train_index], memFC[test_index]
+        ymemtrain, ymemval=ymem[train_index], ymem[test_index]
         semtrain, semval=semFC[train_index], semFC[test_index]
+        ysemtrain, ysemval=ysem[train_index],ysem[test_index]
         mottrain, motval=motFC[train_index], motFC[test_index]
+        ymottrain, ymotval=ymot[train_index],ymot[test_index]
         glatrain, glaval=glassFC[train_index], glassFC[test_index]
+        yglatrain,yglaval=yglass[train_index],yglass[test_index]
         Xtrain_task=np.concatenate((memtrain,semtrain,mottrain,glatrain))
+        ytrain_task=np.concatenate((ymemtrain,ysemtrain,ymottrain,yglatrain))
+        yval_task=np.concatenate((ymemval,ysemval,ymotval,yglaval))
         Xtrain_rest, Xval_rest=restFC[train_index,:,:], restFC[test_index,:,:]
         Xval_task=np.concatenate((memval,semval,motval,glaval))
-        Xtrain_rest=np.reshape(Xtrain_rest,(-1,number))
-        Xval_rest=np.reshape(Xval_rest,(-1,number))
-        ytrain_task = np.ones(Xtrain_task.shape[0], dtype = int)
+        #Xtrain_rest=np.reshape(Xtrain_rest,(-1,number))
+        #Xval_rest=np.reshape(Xval_rest,(-1,number))
+
+        #ytrain_task = np.ones(Xtrain_task.shape[0], dtype = int)
         ytrain_rest=np.zeros(Xtrain_rest.shape[0], dtype=int)
-        yval_task = np.ones(Xval_task.shape[0], dtype = int)
+        #yval_task = np.ones(Xval_task.shape[0], dtype = int)
         yval_rest=np.zeros(Xval_rest.shape[0], dtype=int)
         X_tr=np.concatenate((Xtrain_task, Xtrain_rest))
         y_tr = np.concatenate((ytrain_task,ytrain_rest))
+        X_val=np.concatenate((Xval_task, Xval_rest))
+        y_val = np.concatenate((yval_task,yval_rest))
         clf.fit(X_tr,y_tr)
-        CV_Tscore=clf.score(Xval_task, yval_task)
-        CVTacc.append(CV_Tscore)
-        CV_Rscore=clf.score(Xval_rest, yval_rest)
-        CVRacc.append(CV_Rscore)
-        scoreT=clf.score(test_taskFC, testT)
+        score=clf.score(X_val, y_val)
+        CVTacc.append(score)
+        #CV_Tscore=clf.score(Xval_task, yval_task)
+        #CVTacc.append(CV_Tscore)
+        #CV_Rscore=clf.score(Xval_rest, yval_rest)
+        #CVRacc.append(CV_Rscore)
+        scoreT=clf.score(testFC,ytest)
         DSTacc.append(scoreT)
-        scoreR=clf.score(test_restFC, testR)
-        DSRacc.append(scoreR)
+        #scoreR=clf.score(test_restFC, testR)
+        #DSRacc.append(scoreR)
 
     same_Tsub=mean(CVTacc)
-    same_Rsub=mean(CVRacc)
+    #same_Rsub=mean(CVRacc)
     diff_Tsub=mean(DSTacc)
-    diff_Rsub=mean(DSRacc)
+    #diff_Rsub=mean(DSRacc)
     return same_Tsub, same_Rsub, diff_Tsub, diff_Rsub
+
+
+
+
+
+def multiclassAll():
+    """
+    Preparing machine learning model with appropriate data
+
+    Parameters
+    -------------
+    train_sub : str
+            Subject name for training
+    test_sub : str
+            Subject name for testing
+
+    Returns
+    -------------
+    total_score : float
+            Average accuracy of all folds
+
+    """
+    #clf=LinearSVC()
+    #clf=LogisticRegression(solver = 'lbfgs')
+    clf=RidgeClassifier()
+    #train sub
+    master_df=pd.DataFrame()
+    d=pd.DataFrame()
+    data=np.array(['MSC01','MSC02','MSC03','MSC04','MSC05','MSC06','MSC07','MSC10'],dtype='<U61')
+    loo = LeaveOneOut()
+    for  test, train in loo.split(data): #train on one sub test on the rest
+        tmp=pd.DataFrame()
+        train_sub=data[train]
+        test_sub=data[test]
+    #train sub
+        memFC=reshape.matFiles(dataDir+'mem/'+train_sub[0]+'_parcel_corrmat.mat')
+        semFC=reshape.matFiles(dataDir+'semantic/'+train_sub[0]+'_parcel_corrmat.mat')
+        glassFC=reshape.matFiles(dataDir+'glass/'+train_sub[0]+'_parcel_corrmat.mat')
+        motFC=reshape.matFiles(dataDir+'motor/'+train_sub[0]+'_parcel_corrmat.mat')
+        restFC=reshape.matFiles(dataDir+'rest/corrmats_timesplit/fourths/'+train_sub[0]+'_parcel_corrmat.mat') #keep tasks seperated in order to collect the right amount of days
+        #nsize=restFC.shape[1]
+        #restFC=np.reshape(restFC,(10,4,nsize)) #reshape to gather correct days
+        #test sub
+        testFC,ytest=AllSubFiles(test_sub)
+        same_Tsub, diff_Tsub,f=K_folds(train_sub, clf, memFC,semFC,glassFC,motFC, restFC, testFC, ytest)
+        tmp['train']=train_sub
+        tmp['same_sub']=same_Tsub
+        tmp['diff_sub']=diff_Tsub
+        #tmp['f1_score']=f
+        master_df=pd.concat([master_df,tmp])
+    return f
+    #master_df.to_csv(outDir+'ALL/multiclass_acc.csv',index=False)
+
+def K_folds(train_sub, clf, memFC,semFC,glassFC,motFC, restFC, testFC,ytest):
+    """
+    Cross validation to train and test using nested loops
+
+    Parameters
+    -----------
+    clf : obj
+        Machine learning algorithm
+    taskFC, restFC, test_taskFC, test_restFC : array_like
+        Input arrays, training and testing set of task and rest FC
+    Returns
+    -----------
+    total_score : float
+        Average accuracy across folds
+    acc_score : list
+        List of accuracy for each outer fold
+    """
+
+    loo = LeaveOneOut()
+    number=memFC.shape[1]
+    #test_taskSize=test_taskFC.shape[0]
+    #test_restSize=test_restFC.shape[0]
+    #testT= np.ones(test_taskSize, dtype = int)
+    #testR= np.zeros(test_restSize, dtype = int)
+    yrest=np.zeros(restFC.shape[0])
+    ymem=np.ones(memFC.shape[0])
+    ysem=np.full(semFC.shape[0],2)
+    yglass=np.full(glassFC.shape[0],3)
+    ymot=np.full(motFC.shape[0],4)
+    CVTacc=[]
+    CVRacc=[]
+    df=pd.DataFrame()
+    DSTacc=[]
+    DSRacc=[]
+    f1=[]
+    #fold each training set
+    session=splitDict[train_sub[0]]
+    split=np.empty((session, 55278))
+    for train_index, test_index in loo.split(split):
+        memtrain, memval=memFC[train_index], memFC[test_index]
+        ymemtrain, ymemval=ymem[train_index], ymem[test_index]
+        semtrain, semval=semFC[train_index], semFC[test_index]
+        ysemtrain, ysemval=ysem[train_index],ysem[test_index]
+        mottrain, motval=motFC[train_index], motFC[test_index]
+        ymottrain, ymotval=ymot[train_index],ymot[test_index]
+        glatrain, glaval=glassFC[train_index], glassFC[test_index]
+        yglatrain,yglaval=yglass[train_index],yglass[test_index]
+        resttrain, restval=restFC[train_index], restFC[test_index]
+        yresttrain, yrestval=yrest[train_index],yrest[test_index]
+        Xtrain=np.concatenate((resttrain,memtrain,semtrain,mottrain,glatrain))
+        ytrain=np.concatenate((yresttrain,ymemtrain,ysemtrain,ymottrain,yglatrain))
+        yval=np.concatenate((yrestval,ymemval,ysemval,ymotval,yglaval))
+        Xval=np.concatenate((restval,memval,semval,motval,glaval))
+
+        clf.fit(Xtrain,ytrain)
+        score=clf.score(Xval, yval)
+        CVTacc.append(score)
+        y_predict=clf.predict(Xval)
+        score_test = f1_score(yval, y_predict, average=None)
+        #order is position rest, mem, sem, mot, glass
+        f1.append(score_test)
+        #CV_Tscore=clf.score(Xval_task, yval_task)
+        #CVTacc.append(CV_Tscore)
+        #CV_Rscore=clf.score(Xval_rest, yval_rest)
+        #CVRacc.append(CV_Rscore)
+        scoreT=clf.score(testFC,ytest)
+        DSTacc.append(scoreT)
+        #scoreR=clf.score(test_restFC, testR)
+        #DSRacc.append(scoreR)
+    #f=mean(f1)
+    same_Tsub=mean(CVTacc)
+    #same_Rsub=mean(CVRacc)
+    diff_Tsub=mean(DSTacc)
+    #diff_Rsub=mean(DSRacc)
+    return same_Tsub,diff_Tsub,score_test
